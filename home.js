383cd7d4583461e22c40ae5d33f6370b0dfbd719
gabriellekaye <gabrielle_naval@dlsu.edu.ph>
2022-01-30 16:52:11 +0800

    updated

--- home.js
+++
@@ -1,8 +0,0 @@
-const router = require('express')();
-const { isPrivate } = require('../middlewares/authentication');
-
-router.get('/', isPrivate, (req, res) => {
-  res.render('home', { pageTitle: 'Home', name: req.session.username } );
-});
-
-module.exports = router;
