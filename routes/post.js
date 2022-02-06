37a671894acf25a56157bc84f1b2d98dbcc7742b
gabriellekaye <gabrielle_naval@dlsu.edu.ph>
2022-02-06 19:30:25 +0800

    updated post routes

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+// All these pages will be private
 const { isPrivate } = require('../middlewares/authentication');
 
 //Require the controller to use it
 const recipeController = require('../controller/recipeController');
 
-//Route to homepage
+///Route to homepage
 router.get('/', isPrivate, recipeController.home);
 
 //Submit forms
@@ -22,13 +23,13 @@ router.get('/random-recipe', isPrivate, recipeController.showRandom);
 //To search for a recipe/post based on title
 router.post('/search', isPrivate, recipeController.searchRecipe);
 
-// //To delete a recipe/post
-router.delete('/delete/:id', isPrivate, recipeController.deleteRecipe);
+//To delete a recipe/post
+router.post('/delete/:id', isPrivate, recipeController.deleteRecipe);
 
 //To update a recipe/post
 router.get('/update/:id', isPrivate, recipeController.updateRecipe);
 
-// to show updated recipe
+//To show updated recipe/post
 router.post('/update/:id', isPrivate, recipeController.updateRecipeDone);
 
 //To comment on a recipe
