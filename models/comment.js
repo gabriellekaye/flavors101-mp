f5d9446bb52b6c5faded96cbc33c3667ce80fada
Alyssa Santos <alyssa_marie_santos@dlsu.edu.ph>
2022-02-10 19:27:48 +0800

    added comments with semi reply

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    text: {type : String},
+    user_id: {type: String, require, ref: 'user'},
+    recipe: {type: mongoose.ObjectId, require: true, ref: 'Recipe'},
+    reply_to: {type: mongoose.ObjectId, require: true, ref: 'Comment', default: null}
+})
+
+
+
+module.exports = mongoose.model('Comment', commentSchema);
