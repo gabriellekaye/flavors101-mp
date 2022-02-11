4475ebe8b6dd4d697cd4bfdff58f43587125b199
gabriellekaye <gabrielle_naval@dlsu.edu.ph>
2022-02-11 21:06:45 +0800

    started rates

---
+++ public/javascript/star-rating.js
@@ -0,0 +1,11 @@
+const rating = document.getElementById('stars')
+
+function addRate() {
+    var n = 5;
+    var stared;
+    for(var i=0; i < n; i++) {
+        if (star[i].checked){
+            started++
+        }
+    }
+}
--- validators.js
+++ validators.js
@@ -2,7 +2,7 @@ const {body} = require('express-validator');
 
 const registerValidation = [
   // Username cant be empty
-  body('username').not().isEmpty().withMessage("Username is required."),
+  body('username').not().isEmpty().withMessage("Please enter a username."),
 
   // Username should be at least 6 chars
   body('username').isLength({ min: 6 }).withMessage("Username must be at least 6 characters."),
@@ -22,7 +22,7 @@ const registerValidation = [
   // Avatar is required
   body('avatar').custom((value, {req}) => {
       if(value == null && req.files == null){
-        throw new Error("Avatar is required.");
+        throw new Error("Please upload an avatar.");
       }
       return true;
     })
