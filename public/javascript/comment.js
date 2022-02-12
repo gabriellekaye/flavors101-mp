129beca0796fff0730d0bbe3d7e7f05633108b3c
Alyssa Santos <alyssa_marie_santos@dlsu.edu.ph>
2022-02-12 12:36:43 +0800

    added comment.js in javascript

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,10 +1,24 @@
+$(document).ready(function () {
 
-$("#delete").click( function () {
-    const id = $("#cmt-id").val();
-
-    $.post('/delete-comment', {id: id}, (isSuccess) => {
-        if (isSuccess) {
-            $("#cmt-id").remove();
-        }
+    $("#delete").click( function () {
+        const id = $("#cmt-id").val();
+    
+        $.post('/:id/delete-comment', {id: id}, (isSuccess) => {
+            if (isSuccess) {
+                $("#cmt-id").remove();
+            }
+        });
     });
-});
+
+    $("#edit").click( function () {
+        const id = $("#cmt-id").val();
+        const text = $("#my_new_cmt").val();
+        
+        
+        $.get('/:id/edit', {id: id}, (isSuccess) => {
+           if (isSuccess) {
+            window.location('/:id/edit');
+           }
+        });
+    });
+})
